8cdecf2d32c99edf43d18baa7587671c1f285d82
Quang Phan <quang.phan@tallygroup.com.au>
2023-05-18 22:28:13 +0700

    custom login page

--- src/megax/MegaApp/nextapp/app/(auth)/login/page.tsx
+++ src/megax/MegaApp/nextapp/app/(auth)/login/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import Button from "@mui/material/Button";
 import { signIn, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
+
+import Button from "@mui/material/Button";
+import GoogleIcon from "@mui/icons-material/Google";
+import { Typography } from "@mui/material";
 
 export default function SignInPage() {
   const session = useSession();
@@ -13,12 +16,26 @@ export default function SignInPage() {
   const provider = "google"; // currently we only support google
 
   return (
-    <>
-      <div>Signin</div>
+    <div
+      className="flex items-center justify-center"
+      style={{ minHeight: "calc(100vh - 50px)" }}
+    >
+      <div className="border rounded-lg shadow p-8 bg-blue-100">
+        <Typography component="div" variant="h4">
+          Sign In to MegaX
+        </Typography>
 
-      <Button variant="contained" onClick={() => signIn(provider)}>
-        Sign in
-      </Button>
-    </>
+        <div className="mt-4">
+          <Button
+            variant="contained"
+            className="!bg-blue-500"
+            onClick={() => signIn(provider)}
+          >
+            <GoogleIcon className="mr-4" />
+            Sign in using Google
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
