6d563fceac264f21dfa948e5eb7b5122805e3356
Quang Phan <quang.phan@tallygroup.com.au>
2023-05-19 10:04:23 +0700

    update rewrite rules

--- src/megax/MegaApp/nextapp/next.config.js
+++ src/megax/MegaApp/nextapp/next.config.js
@@ -3,18 +3,29 @@
 const isProd = process.env.NODE_ENV === "production";
 const backendUrl = process.env.BACKEND_URL;
 
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 if (isProd) {
   nextConfig.output = "export";
 } else {
   nextConfig.rewrites = async () => [
+    // {
+    //   source: "/api/auth/:path*", // use as below
+    //   destination: "/api/auth/:path*",
+    // },
     {
-      source: "/api/auth/:path*", // keep the auth path
-      destination: "/api/auth/:path*",
-    },
-    {
-      source: "/api/:path*",
+      source: "/api/:path((?!auth).*)", // skip auth path for authentication
       destination: backendUrl + "/api/:path*",
     },
   ];
