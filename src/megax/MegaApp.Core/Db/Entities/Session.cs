7a79409594e931149ce9531944557d1fc4f7ed26
Quang Phan <quang.phan@tallygroup.com.au>
2023-05-20 16:21:05 +0700

    add db

---
+++ src/megax/MegaApp.Core/Db/Entities/Session.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MegaApp.Core.Db.Entities
+{
+    public class Session
+    {
+        public Guid Id { get; set; }
+        public DateTime? Expires { get; set; }
+        public string SessionToken { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+    }
+
+    public class SessionConfiguration : IEntityTypeConfiguration<Session>
+    {
+        public void Configure(EntityTypeBuilder<Session> builder)
+        {
+            builder.ToTable("Sessions", "auth");
+        }
+    }
+}
---
+++ src/megax/MegaApp.Core/Db/Entities/User.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MegaApp.Core.Db.Entities
+{
+    public class User : ICreatedByEntity, IUpdatedByEntity
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public DateTime? EmailVerified { get; set; }
+
+        /// <summary>
+        /// profile image
+        /// </summary>
+        public string Image { get; set; }
+
+        public bool IsDisabled { get; set; }
+
+        public List<Account> Accounts { get; set; } = new();
+        public List<Session> Sessions { get; set; } = new();
+
+        public DateTimeOffset CreatedAt { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+        public Guid? UpdatedBy { get; set; }
+    }
+
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasMany(x => x.Accounts)
+                .WithOne(a => a.User)
+                .HasForeignKey(a => a.UserId);
+
+            builder.HasMany(x => x.Sessions)
+                .WithOne(s => s.User)
+                .HasForeignKey(s => s.UserId);
+
+            builder.Property(x => x.CreatedBy)
+                .HasDefaultValueSql("sysdatetimeoffset()");
+        }
+    }
+}
