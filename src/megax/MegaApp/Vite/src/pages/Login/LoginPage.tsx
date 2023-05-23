3e55f8e8db264e6f7f33fb8ba689aa71123b1be8
Stephen Nguyen <tungbinhdinh89@gmail.com>
2023-05-23 23:10:39 +0700

    using useForm repalce for useFormik

--- src/megax/MegaApp/Vite/src/pages/Login/LoginPage.tsx
+++ src/megax/MegaApp/Vite/src/pages/Login/LoginPage.tsx
@@ -1,64 +1,72 @@
 import TextField from "@mui/material/TextField";
 import Alert from "@mui/material/Alert";
 import Button from "@mui/material/Button";
-import { useFormik } from "formik";
+import { useForm } from "react-hook-form";
 import { useState } from "react";
 
 function LoginPage() {
-  const defaultValue = {
-    userName: "thanhtung",
-    passworld: "10091989",
-  };
   const [isSuccess, setIsSuccess] = useState(false);
   const [isFailed, setIsFailed] = useState(false);
 
-  const form = useFormik({
-    initialValues: {
-      username: "",
-      password: "",
-    },
+  const defaultValue = {
+    userName: "khangnguyen2019@gmail.com",
+    passworld: "10091989",
+  };
 
-    onSubmit: values => {
-      console.log(values);
-      values.username === defaultValue.userName &&
-      values.password === defaultValue.passworld
-        ? setIsSuccess(true)
-        : setIsFailed(true);
-    },
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   return (
     <div className="w-full">
       <form
+        onSubmit={handleSubmit(values => {
+          console.log(values);
+          values.username === defaultValue.userName &&
+          values.password === defaultValue.passworld
+            ? setIsSuccess(true)
+            : setIsFailed(true);
+        })}
         className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        onSubmit={form.handleSubmit}
       >
         <div className=" mb-4">
           <TextField
-            name="username"
-            value={form.values.username}
-            onChange={form.handleChange}
+            {...register("username", {
+              required: "Thi is required.",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Username must be a valid email",
+              },
+            })}
             size="medium"
             style={{ width: 400 }}
             id="name"
             type="text"
             label="Username"
           />
+          <p className="text-red-500 text-xs italic mt-3 text-left">
+            {errors.username?.message}
+          </p>
         </div>
         <div className="mb-6">
           <TextField
+            {...register("password", {
+              required: "Thi is required.",
+              minLength: {
+                value: 6,
+                message: "Password must greater than 6 characters",
+              },
+            })}
             size="medium"
             style={{ width: 400 }}
-            name="password"
-            value={form.values.password}
-            onChange={form.handleChange}
-            id="outlined-password-input"
+            id="password"
             label="Password"
             type="password"
-            autoComplete="current-password"
           />
           <p className="text-red-500 text-xs italic mt-3 text-left">
-            Please choose a password.
+            {errors.password?.message}
           </p>
         </div>
         <div className="flex flex-col items-start justify-between">
