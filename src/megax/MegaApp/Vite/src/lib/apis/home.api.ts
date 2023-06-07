6766c03ad2994ddc1a3b1a1231947057072d198d
Stephen Nguyen <tungbinhdinh89@gmail.com>
2023-06-07 21:54:38 +0700

    delete file unuse

--- src/megax/MegaApp/Vite/src/lib/apis/home.api.ts
+++
@@ -1,8 +0,0 @@
-import { Forecast } from "../models/home.model";
-import api from "./axios.instance";
-
-export async function fetchWeatherForecast() {
-  const response = await api.get<Forecast[]>("/api/weatherforecast");
-
-  return response.data;
-}
