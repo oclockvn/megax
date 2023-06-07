b3c465445cefe6a2bde2ed64027b16d0a29e1af9
Stephen Nguyen <tungbinhdinh89@gmail.com>
2023-06-07 22:08:47 +0700

    renew file

---
+++ src/megax/MegaApp/Vite/src/lib/apis/axios.instance.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import storage from "../storage";
+
+const api = axios.create({
+
+    headers: {
+        "Content-type": "application/json",
+        Accept: "application/json",
+    },
+});
+
+api.interceptors.request.use(function (config) {
+    const token = storage.get("token");
+    console.log("token", token);
+    config.headers.Authorization = `Bearer ${token}`;
+
+    return config;
+});
+
+export default api;
