88bb72376f28ad2b2b012f90677b3bb91c0680b4
Stephen Nguyen <tungbinhdinh89@gmail.com>
2023-06-08 11:28:14 +0700

    add react-toast lib

--- src/megax/MegaApp/Vite/src/App.tsx
+++ src/megax/MegaApp/Vite/src/App.tsx
@@ -1,12 +1,26 @@
 import "./App.css";
 import AuthProvider from "./provider/authProvider";
 import Routes from "./routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
       <AuthProvider>
         <Routes />
+        <ToastContainer
+          position="top-right"
+          autoClose={1000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
       </AuthProvider>
     </>
   );
