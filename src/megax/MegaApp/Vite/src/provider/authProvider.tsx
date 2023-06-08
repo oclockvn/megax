0f396dbaa389786a1802dae670c3202028ef1c16
Stephen Nguyen <tungbinhdinh89@gmail.com>
2023-06-08 11:33:48 +0700

    delete function add and remove token in useEffect

--- src/megax/MegaApp/Vite/src/provider/authProvider.tsx
+++ src/megax/MegaApp/Vite/src/provider/authProvider.tsx
@@ -10,16 +10,6 @@ const AuthProvider = ({ children }) => {
     setToken_(newToken);
   };
 
-  useEffect(() => {
-    if (token) {
-      axios.defaults.headers.common["Authorization"] = "Bearer " + token;
-      localStorage.setItem("token", token);
-    } else {
-      delete axios.defaults.headers.common["Authorization"];
-      localStorage.removeItem("token");
-    }
-  }, [token]);
-
   const contextValue = useMemo(
     () => ({
       token,
