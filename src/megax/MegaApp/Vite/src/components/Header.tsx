a5d527545adc5aed2d1842e6b22d2b595051d42b
Stephen Nguyen <tungbinhdinh89@gmail.com>
2023-06-09 12:01:57 +0700

    edit header component

--- src/megax/MegaApp/Vite/src/components/Header.tsx
+++ src/megax/MegaApp/Vite/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -5,9 +6,33 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { Link, useNavigate } from "react-router-dom";
 import storage from "../lib/storage";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
 
 function Header() {
   const token = storage.get("token");
+  const pages = token
+    ? [
+        { id: 1, name: "Profile", to: "/profile" },
+        { id: 2, name: "Profile", to: "/profile" },
+        { id: 3, name: "Profile", to: "/profile" },
+      ]
+    : [
+        { id: 1, name: "About", to: "/about-us" },
+        { id: 2, name: "About", to: "/about-us" },
+        { id: 3, name: "About", to: "/about-us" },
+      ];
+  const settings = [
+    { id: 1, name: "Profile", to: "/profile" },
+    { id: 2, name: "Account", to: "/profile" },
+    { id: 3, name: "Dashboard", to: "/" },
+    { id: 3, name: "Logout", to: "/" },
+  ];
 
   const navigate = useNavigate();
   const handleLogout = () => {
@@ -16,28 +41,179 @@ function Header() {
     navigate(0);
   };
 
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar
-          position="static"
-          style={{ background: "white", color: "#1181e1" }}
-        >
-          <Toolbar variant="dense" color="yellow">
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <Link to={"/"}>MegaX</Link>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                // fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              MegaX
             </Typography>
-            <Button color="inherit">
-              <Link to={"/about-us"}>About</Link>
-            </Button>
-            {token && (
-              <Button color="inherit" onClick={handleLogout}>
-                Logout
-              </Button>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map(page => (
+                  <MenuItem key={page.id} onClick={handleCloseNavMenu}>
+                    <Link to={page.to}>
+                      <Typography textAlign="center">{page.name}</Typography>
+                    </Link>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                // fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              MegaX
+            </Typography>
+            <Box
+              sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+              style={{ justifyContent: "flex-end" }}
+            >
+              {pages.map(page => (
+                <Link to={page.to} key={page.id}>
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: "white", display: "block" }}
+                  >
+                    {page.name}
+                  </Button>
+                </Link>
+              ))}
+            </Box>
+
+            {token ? (
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt="Remy Sharp"
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map(setting => (
+                    <MenuItem key={setting.id} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{setting.name}</Typography>
+                    </MenuItem>
+                  ))}
+                  {/* <MenuItem onClick={handleCloseUserMenu}>
+                    <div className="flex flex-col justify-start items-start">
+                      {settings.map(setting => (
+                        <React.Fragment key={setting.id}>
+                          <Link to={setting.to}>
+                            <Typography textAlign="center">
+                              {setting.name}
+                            </Typography>
+                          </Link>
+                        </React.Fragment>
+                      ))}
+                      <Typography textAlign="center" onClick={handleLogout}>
+                        Logout
+                      </Typography>
+                    </div>
+                  </MenuItem> */}
+                </Menu>
+              </Box>
+            ) : (
+              <Link to="/signup">
+                <Button style={{ color: "white" }}>Sign Up</Button>
+              </Link>
             )}
           </Toolbar>
-        </AppBar>
-      </Box>
+        </Container>
+      </AppBar>
     </>
   );
 }
