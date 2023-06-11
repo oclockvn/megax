534adf25206bacee1fd290244c4ef57543906232
Quang Phan <quang.phan@tallygroup.com.au>
2023-06-11 22:19:42 +0700

    provide session from server

--- src/megax/MegaApp/nextapp/app/(portal)/layout.tsx
+++ src/megax/MegaApp/nextapp/app/(portal)/layout.tsx
@@ -3,6 +3,7 @@ import "../globals.css";
 import { Roboto } from "next/font/google";
 import AuthProvider from "@/components/AuthProvider";
 import StateProvider from "@/lib/store/state.provider";
+import { getServerSession } from "next-auth/next";
 
 const roboto = Roboto({
   weight: ["100", "300", "400", "700"],
@@ -14,16 +15,18 @@ export const metadata = {
   description: "A Mega App for A Company",
 };
 
-export default function PortalLayout({
+export default async function PortalLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession();
+
   return (
     <html lang="en">
       <body className={roboto.className}>
         <StateProvider>
-          <AuthProvider>
+          <AuthProvider session={session}>
             <Nav />
             {children}
           </AuthProvider>
