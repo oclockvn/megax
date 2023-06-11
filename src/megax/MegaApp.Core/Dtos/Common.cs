ade01b500fe96a4cd9edc1455a896e4eddb26555
Quang Phan <quang.phan@tallygroup.com.au>
2023-06-11 22:16:04 +0700

    default filter

--- src/megax/MegaApp.Core/Dtos/Common.cs
+++ src/megax/MegaApp.Core/Dtos/Common.cs
@@ -5,4 +5,6 @@ public record Filter
     public string Query { get; set; }
     public string OrderBy { get; set; }
     public bool IsAsc { get; set; }
+    public int Page { get; set; } = 0;
+    public int PageSize { get; set; } = 100;
 }
