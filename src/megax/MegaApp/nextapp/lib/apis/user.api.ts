fceb8c9dc5c3530cb4af6d70408b81048c26bac7
Quang Phan <quang.phan@tallygroup.com.au>
2023-06-11 09:26:21 +0700

    rename client to api

---
+++ src/megax/MegaApp/nextapp/lib/apis/user.api.ts
@@ -0,0 +1,13 @@
+import api from "@/lib/api";
+import { Result } from "@/lib/models/common.model";
+import { User } from "@/lib/models/user.model";
+
+export async function getUserList() {
+  const res = await api.get<User[]>("/be/user");
+  return res.data;
+}
+
+export async function updateUserDetail(user: User) {
+  const res = await api.post<Result<User>>("/be/user/" + user.id, { user });
+  return res.data;
+}
