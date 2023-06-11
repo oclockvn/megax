36ab3638b300d8db4fd711e305a2aeb425c0eaa7
Quang Phan <quang.phan@tallygroup.com.au>
2023-06-11 22:20:40 +0700

    ignore serialization check

--- src/megax/MegaApp/nextapp/lib/store/store.ts
+++ src/megax/MegaApp/nextapp/lib/store/store.ts
@@ -7,6 +7,10 @@ export const store = configureStore({
     weather: weatherStore,
     user: userStore,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
