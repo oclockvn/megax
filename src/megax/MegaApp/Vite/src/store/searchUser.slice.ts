cbea2b6ef9ce5a22da4dcf8ae0c6e8f1f33928dd
Stephen Nguyen <tungbinhdinh89@gmail.com>
2023-06-14 14:58:29 +0700

    add search users slice

---
+++ src/megax/MegaApp/Vite/src/store/searchUser.slice.ts
@@ -0,0 +1,36 @@
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { fetchSearchUsers } from '../lib/apis/users.api'
+
+import { UsersInfo } from "../lib/models/users.model";
+
+export interface UsersState {
+    items: UsersInfo[];
+}
+
+export const getSearchUsersThunk = createAsyncThunk(
+    "usersPage/searchUser",
+    async keyword => {
+        return await fetchSearchUsers(keyword);
+    }
+);
+
+const usersState: UsersState = {
+    items: [],
+};
+
+export const searchUserSlice = createSlice({
+    name: "searchUsers",
+    initialState: usersState,
+    reducers: {
+
+    },
+    extraReducers: builder =>
+        builder.addCase(
+            getSearchUsersThunk.fulfilled,
+            (state: UsersState, action: PayloadAction<UsersInfo[]>) => {
+                const { payload: items } = action;
+                state.items = items;
+            }
+        ),
+});
+export default searchUserSlice.reducer;
