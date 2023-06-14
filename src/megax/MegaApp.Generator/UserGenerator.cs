a0c64ff16fdee8e823ca7ce12e5f32441bc29a73
Quang Phan <79438276+quangphantally@users.noreply.github.com>
2023-06-14 11:38:33 +0700

    generate users

---
+++ src/megax/MegaApp.Generator/UserGenerator.cs
@@ -0,0 +1,39 @@
+﻿using Bogus;
+using MegaApp.Core.Db;
+using MegaApp.Core.Db.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MegaApp.Generator;
+
+public interface IUserGenerator
+{
+    Task GenerateAsync(int count = 100);
+}
+
+public class UserGenerator : IUserGenerator
+{
+    private readonly IDbContextFactory<ApplicationDbContext> dbContextFactory;
+
+    public UserGenerator(IDbContextFactory<ApplicationDbContext> dbContextFactory)
+    {
+        this.dbContextFactory = dbContextFactory;
+    }
+
+    private ApplicationDbContext UseDb() => dbContextFactory.CreateDbContext();
+
+    public async Task GenerateAsync(int count = 100)
+    {
+        var faker = new Faker<User>()
+            .RuleFor(r => r.FullName, f => f.Name.FullName())
+            .RuleFor(r => r.Email, f => f.Internet.Email())
+            .RuleFor(r => r.Address, f => f.Address.FullAddress())
+            .RuleFor(r => r.IdentityNumber, f => f.Commerce.Ean13())
+            .RuleFor(r => r.Phone, f => f.Phone.PhoneNumber())
+            .RuleFor(r => r.Dob, f => f.Date.Between(new DateTime(1980, 1, 1), new DateTime(2000, 1, 1)));
+
+        var users = faker.Generate(Math.Min(count, 1000));
+        using var db = UseDb();
+        await db.Users.AddRangeAsync(users);
+        await db.SaveChangesAsync();
+    }
+}
---
+++ src/megax/MegaApp/Controllers/GeneratorController.cs
@@ -0,0 +1,25 @@
+﻿using MegaApp.Generator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MegaApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GeneratorController : ApplicationControllerBase
+{
+    private readonly IUserGenerator userGenerator;
+
+    public GeneratorController(
+        IUserGenerator userGenerator
+        )
+    {
+        this.userGenerator = userGenerator;
+    }
+
+    [HttpPost("users")]
+    public async Task<IActionResult> GenerateUsers([FromQuery] int count = 100)
+    {
+        await userGenerator.GenerateAsync(count);
+        return Ok(new { success = true });
+    }
+}
