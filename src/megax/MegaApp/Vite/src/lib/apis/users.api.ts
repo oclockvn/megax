d9f4d92d8316dd9ebcb229658fd3c4da1c87635c
Stephen Nguyen <tungbinhdinh89@gmail.com>
2023-06-14 14:54:27 +0700

    add get user list api

---
+++ src/megax/MegaApp/Vite/src/lib/apis/users.api.ts
@@ -0,0 +1,19 @@
+import { UsersInfo } from "../models/users.model";
+import api from "./axios.instance";
+
+export async function fetchUsersInfo() {
+    const response = await api.get<UsersInfo[]>("/api/users");
+
+    return response.data.items;
+}
+
+
+export async function fetchSearchUsers(keyword: string) {
+    const response = await api.get<UsersInfo[]>(`api/users?query=${keyword}`)
+    return response.data.items
+}
+
+export async function fetchPageUsers(page: string) {
+    const response = await api.get<UsersInfo[]>(`api/users?page=${page}`)
+    return response.data.items
+}
