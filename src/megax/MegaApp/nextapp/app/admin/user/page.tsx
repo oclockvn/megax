d8ee06aaa59383bbafdd8d15b084aa7baa4856a9
Quang Phan <quang.phan@tallygroup.com.au>
2023-06-18 09:27:43 +0700

    refactor custom grid ui

--- src/megax/MegaApp/nextapp/app/admin/user/page.tsx
+++ src/megax/MegaApp/nextapp/app/admin/user/page.tsx
@@ -1,59 +1,20 @@
 "use client";
 
-import * as React from "react";
-import {
-  DataGrid,
-  GridColDef,
-  GridPagination,
-  useGridApiContext,
-  useGridSelector,
-  gridRowCountSelector,
-  gridPageSizeSelector,
-} from "@mui/x-data-grid";
+import { useState, useEffect } from "react";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import { useAppDispatch, useAppSelector } from "@/lib/store/state.hook";
 import { fetchUsersThunk } from "@/lib/store/user.state";
 import datetime from "@/lib/datetime";
 import { PageModel } from "@/lib/models/common.model";
 
-import MuiPagination from "@mui/material/Pagination";
-import { TablePaginationProps } from "@mui/material/TablePagination";
-import TextField from "@mui/material/TextField";
-
-function Pagination({
-  page,
-  onPageChange,
-  className,
-}: Pick<TablePaginationProps, "page" | "onPageChange" | "className">) {
-  const apiRef = useGridApiContext();
-  const rowCount = useGridSelector(apiRef, gridRowCountSelector);
-  const pageSize = useGridSelector(apiRef, gridPageSizeSelector);
-  const pageCount = Math.ceil(rowCount / pageSize);
-
-  return React.useMemo(
-    () => (
-      <MuiPagination
-        color="primary"
-        className={className}
-        count={pageCount}
-        page={page + 1}
-        onChange={(event, newPage) => {
-          onPageChange(event as any, newPage - 1);
-        }}
-      />
-    ),
-    [rowCount, pageSize, pageCount, page]
-  );
-}
-
-function CustomPagination(props: any) {
-  return <GridPagination ActionsComponent={Pagination} {...props} />;
-}
+import CustomPagination from "@/components/grid/CustomPagination";
+import CommonSearch from "@/components/grid/CommonSearch";
 
 export default function UserListPage() {
   const appDispatch = useAppDispatch();
   const { isLoading, pagedUsers } = useAppSelector(s => s.user);
-  const [pageModel, setPageModel] = React.useState(new PageModel(0, 100));
-  const [query, setQuery] = React.useState("");
+  const [pageModel, setPageModel] = useState(new PageModel(0, 100));
+  const [query, setQuery] = useState("");
 
   const columns: GridColDef[] = [
     { field: "fullName", headerName: "Full Name", width: 400 },
@@ -81,15 +42,7 @@ export default function UserListPage() {
     });
   };
 
-  const handleSearch = (
-    ev: React.KeyboardEvent & React.ChangeEvent<HTMLInputElement>
-  ) => {
-    if (ev.key === "Enter") {
-      setQuery(ev.target?.value);
-    }
-  };
-
-  React.useEffect(() => {
+  useEffect(() => {
     appDispatch(
       fetchUsersThunk({
         page: pageModel.page,
@@ -99,20 +52,14 @@ export default function UserListPage() {
   }, [pageModel.page, query]);
 
   // for initial load
-  React.useEffect(() => {
+  useEffect(() => {
     onPaging(new PageModel());
   }, []);
 
   return (
     <div className="p-4 min-h-[400px]">
       <div className="mb-4">
-        <TextField
-          label="Search"
-          variant="outlined"
-          placeholder="Type search term and enter to search"
-          className="min-w-[400px]"
-          onKeyUp={handleSearch}
-        />
+        <CommonSearch handleSearch={setQuery} />
       </div>
 
       <DataGrid
@@ -134,7 +81,6 @@ export default function UserListPage() {
         pageSizeOptions={[100]}
         loading={isLoading}
         className="min-h-[400px]"
-        // checkboxSelection
       />
     </div>
   );
