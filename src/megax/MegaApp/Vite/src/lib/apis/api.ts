d8b24fbaf894206499dddca3a2d50ac110750699
Stephen Nguyen <tungbinhdinh89@gmail.com>
2023-06-28 15:19:57 +0700

    config interceptor when token expired

--- src/megax/MegaApp/Vite/src/lib/apis/api.ts
+++ src/megax/MegaApp/Vite/src/lib/apis/api.ts
@@ -3,8 +3,6 @@
 import axios from "axios";
 import storage from "../storage";
 
-
-
 const api = axios.create({
 
     withCredentials: true,
@@ -25,12 +23,41 @@ api.interceptors.request.use(function (config) {
 
 })
 
+api.interceptors.response.use(
+    r => r,
+    err => handleRefreshToken(err)
+);
 
-export function handleResponse(body: any) {
-    if (body == null || typeof body !== "object") {
-        return body;
+function handleRefreshToken(err: any) {
+
+    // console.log("> axios intercepter", {
+    //     status: err.response.status,
+
+    // });
+
+    // if not 401 unauthorized -> skip it
+    // if (!err.response || err.response.status !== 401) {
+    //     return Promise.reject(err);
+    // }
+
+    // forbidden
+    // if (err.response.status === 403 && err.response.data) {
+    //     return Promise.reject(err.response.data);
+    // }
+
+    // Access Token was expired
+    // todo: handle if first request to protected resouces
+    if (err.response.status === 401) {
+        storage.remove("token");
+        window.location.href = "/login";
+
+
     }
+
+    return Promise.reject(err);
 }
+
+
 
 
 export default api;
