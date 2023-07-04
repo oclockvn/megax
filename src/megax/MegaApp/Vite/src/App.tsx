c48aabb255e8ba5f92cd2db7e6252f50065d642e
Stephen Nguyen <tungbinhdinh89@gmail.com>
2023-07-04 08:13:55 +0700

    wrap LocalizatonProvider

--- src/megax/MegaApp/Vite/src/App.tsx
+++ src/megax/MegaApp/Vite/src/App.tsx
@@ -3,27 +3,35 @@ import AuthProvider from "./provider/authProvider";
 import Routes from "./routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import DateFnsProvider from "./provider/DateFnsProvider";
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <Routes />
-        <ToastContainer
-          position="top-right"
-          autoClose={1000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-        />
-      </AuthProvider>
+      <DateFnsProvider>
+        {/* <LocalizationProvider dateAdapter={AdapterDateFns}> */}
+        <AuthProvider>
+          <Routes />
+          <ToastContainer
+            position="top-right"
+            autoClose={1000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
+        </AuthProvider>
+        {/* </LocalizationProvider> */}
+      </DateFnsProvider>
     </>
   );
 }
+
 
 export default App;
