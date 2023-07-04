137366e1abacd44df80da024aec4b448ae1a10f4
Stephen Nguyen <tungbinhdinh89@gmail.com>
2023-07-04 08:18:42 +0700

    install, create and use localization provider

--- src/megax/MegaApp/Vite/src/App.tsx
+++ src/megax/MegaApp/Vite/src/App.tsx
@@ -3,15 +3,12 @@ import AuthProvider from "./provider/authProvider";
 import Routes from "./routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import DateFnsProvider from "./provider/DateFnsProvider";
 
 function App() {
   return (
     <>
       <DateFnsProvider>
-        {/* <LocalizationProvider dateAdapter={AdapterDateFns}> */}
         <AuthProvider>
           <Routes />
           <ToastContainer
@@ -27,7 +24,6 @@ function App() {
             theme="colored"
           />
         </AuthProvider>
-        {/* </LocalizationProvider> */}
       </DateFnsProvider>
     </>
   );
---
+++ src/megax/MegaApp/Vite/src/provider/DateFnsProvider.tsx
@@ -0,0 +1,28 @@
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import {
+  LocalizationProvider,
+  LocalizationProviderProps,
+  MuiPickersAdapter,
+} from "@mui/x-date-pickers";
+
+export type DateFnsProviderProps<TDate> = Omit<
+  LocalizationProviderProps<TDate, any>,
+  "dateAdapter"
+> & {
+  dateAdapter?: new (...args: any) => MuiPickersAdapter<TDate>;
+};
+
+export default function DateFnsProvider({
+  children,
+  ...props
+}: DateFnsProviderProps<Date>) {
+  const { dateAdapter, ...localizationProps } = props;
+  return (
+    <LocalizationProvider
+      dateAdapter={dateAdapter || AdapterDateFns}
+      {...localizationProps}
+    >
+      {children}
+    </LocalizationProvider>
+  );
+}
