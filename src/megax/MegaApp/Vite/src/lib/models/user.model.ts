70cb5f7aee0f4c02a2b25bf026e45fe9b650c58c
Stephen Nguyen <tungbinhdinh89@gmail.com>
2023-07-15 08:34:05 +0700

    add interface user device record

--- src/megax/MegaApp/Vite/src/lib/models/user.model.ts
+++ src/megax/MegaApp/Vite/src/lib/models/user.model.ts
@@ -1,21 +1,11 @@
 export interface User {
-    id: number;
-    email: string;
-    fullName: string;
-    dob?: Date;
-    address: string;
-    phone: string;
-    identityNumber: string;
-}
-
-export interface UsersSearch {
-  page: number;
-  keyword: string;
-}
-
-
-export function EmptyUser(): User {
-  return {} as User;
+  id: number;
+  email: string;
+  fullName: string;
+  dob?: Date;
+  address: string;
+  phone: string;
+  identityNumber: string;
 }
 
 export interface UserDeviceRecord {
@@ -25,4 +15,22 @@ export interface UserDeviceRecord {
   deviceType: string;
   takenAt: Date;
   returnedAt?: Date;
+}
+
+// export interface UserDeviceModel {
+//   id: number;
+//   deviceId: number;
+//   name: string;
+//   deviceType: string;
+//   model: string;
+//   qty: number;
+// }
+
+export interface UsersSearch {
+  page: number;
+  keyword: string;
+}
+
+export function EmptyUser(): User {
+  return {} as User;
 }
