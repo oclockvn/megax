4d5fc63c3f3dc04091320c42928d5502741a7642
Quang Phan <quang.phan@tallygroup.com.au>
2023-08-25 23:20:36 +0700

    rename column

---
+++ src/megax/MegaApp.Core/Migrations/20230825152008_RenameUserColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MegaApp.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameUserColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Notion",
+                table: "Users",
+                newName: "Nation");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nation",
+                table: "Users",
+                newName: "Notion");
+        }
+    }
+}
