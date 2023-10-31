a64a46f97acb098bc74a621165ab3040d71b2d16
Quang Phan <oclockvn@gmail.com>
2023-10-31 15:37:18 +0700

    show server error

--- src/megax/MegaApp/nextapp/lib/helpers/response.ts
+++ src/megax/MegaApp/nextapp/lib/helpers/response.ts
@@ -1,11 +1,17 @@
-export const extractErrors = (response: any, defaultMessage = 'Something went wrong') => {
+export const extractErrors = (response: any, defaultMessage = 'Something went wrong'): string => {
+  console.log(response);
   if (!response || typeof response !== 'object') {
     return defaultMessage;
+  }
+
+  // internal server error
+  if (response.code === 'INTERNAL_SERVER_ERROR') {
+    return response.message as string || defaultMessage;
   }
 
   if (!response.errors || typeof response.errors !== 'object') {
     return defaultMessage;
   }
 
-  return Object.values(response.errors)[0] || defaultMessage;
+  return Object.values(response.errors)[0] as string || defaultMessage;
 }
