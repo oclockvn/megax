fa88d5495e0b4959f1538412252da4ebdbfbe9c7
Quang Phan <oclockvn@gmail.com>
2023-11-05 21:37:01 +0700

    fuzzy search user with fuse

--- src/megax/MegaApp/nextapp/components/common/UserSelector.tsx
+++ src/megax/MegaApp/nextapp/components/common/UserSelector.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CommonSearch from "../grid/CommonSearch";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
@@ -8,40 +8,78 @@ import ListItemButton from "@mui/material/ListItemButton";
 import Checkbox from "@mui/material/Checkbox";
 import ListItemText from "@mui/material/ListItemText";
 import Button from "@mui/material/Button";
+import { useQuery } from "@tanstack/react-query";
+import { fetchUserList } from "@/lib/apis/user.api";
+import Fuse from "fuse.js";
+import { User } from "@/lib/models/user.model";
 
 export default function UserSelector() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [rand, setRand] = useState(0)
+  const [keyword, setKeyword] = useState("");
+
+  const { isLoading, data: source } = useQuery({
+    queryKey: ["users-search", keyword, rand],
+    queryFn: () =>
+      fetchUserList({
+        query: keyword,
+        pageSize: 100,
+        sortBy: "id",
+        sortDir: "desc",
+      }),
+      select: data => data?.items || []
+  });
+
+  const fuse = new Fuse(source || [], {
+    keys: ["fullName", "email"],
+    minMatchCharLength: 2, // ignore single match
+  });
+
+  useEffect(() => {
+    setUsers(source || []);
+  }, [source]);
+
   const handleSearch = (q: string) => {
-    console.log(q);
+    if (!q) {
+      setRand(Date.now())
+      return;
+    }
+
+    const result = fuse.search(q);
+
+    if (result.length) { // use local result
+      setUsers(result.map(r => r.item));
+    } else { // trigger api search
+      setKeyword(q);
+    }
   };
 
   return (
     <div>
-      <CommonSearch
-        keypress
-        label="Member search"
-        handleSearch={handleSearch}
-      />
-      <List
-        disablePadding
-        dense
-        className="my-2 border border-slate-200 rounded"
-      >
-        {[1, 2, 3].map((i, index) => (
-          <ListItem
-            key={i}
-            disablePadding
-            className={index === 0 ? "" : `border-t border-slate-200`}
-          >
-            <ListItemButton role={undefined} dense>
-              <Checkbox edge="start" />
-              <ListItemText>
-                <div>Quang Phan</div>
-                <small>quang@spiderbox.design</small>
-              </ListItemText>
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <CommonSearch label="Member search" handleSearch={handleSearch} />
+      <div>
+        <List
+          disablePadding
+          dense
+          className="my-2 border border-slate-200 rounded max-h-[500px] overflow-auto"
+        >
+          {users.map((user, index) => (
+            <ListItem
+              key={user.id}
+              disablePadding
+              className={index === 0 ? "" : `border-t border-slate-200`}
+            >
+              <ListItemButton role={undefined} dense>
+                <Checkbox edge="start" />
+                <ListItemText>
+                  <div>{user.fullName}</div>
+                  <small>{user.email}</small>
+                </ListItemText>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </div>
 
       <Button size="small" variant="contained">
         Add
